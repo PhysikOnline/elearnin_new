0a5d321e037fca51884c8cd06a83b9a3c216a070
Bomel <keiwanjamaly@protonmail.com>
2019-10-07 01:12:39 +0200

    auto formater :)

--- backend/README.md
+++ backend/README.md
@@ -1,18 +1,25 @@
 # Database Tutorial
-The connetion of the database is specified inside `/sql/db.js`. The database initialisation script is located inside `/sql/init.sql`. To use the Database in you .js files, you simplly do: 
+
+The connetion of the database is specified inside `/sql/db.js`. The database initialisation script is located inside `/sql/init.sql`. To use the Database in you .js files, you simplly do:
+
 ```js
-var sql = require('./sql/db.js');
+var sql = require("./sql/db.js");
 ```
+
 queries can the be executed with:
+
 ```js
-sql.query("you querry", function (error, results, fields) {
-    if (error) throw error;
-})
+sql.query("you querry", function(error, results, fields) {
+  if (error) throw error;
+});
 ```
+
 Documentation for MariaDB(SQL) and the MySQL Connector can be found here:
+
 - https://mariadb.com/kb/en/library/documentation/
 - https://github.com/mysqljs/mysql
 
 # Todo
+
 - Figure out "close connection"
 - Find out, why there are SQL disconnecs after a few minutes
--- backend/sql/db.js
+++ backend/sql/db.js
@@ -1,26 +1,34 @@
-'user strict';
+"user strict";
 
-var mysql = require('mysql');
-var fs = require('fs');
+var mysql = require("mysql");
+var fs = require("fs");
 
 var connection = mysql.createConnection({
-    host: '141.2.246.162',
-    user: 'elearning',
-    port: '3306',
-    password: 'elearningpw',
-    database: 'keiwan',
-    multipleStatements: true
+  host: "141.2.246.162",
+  user: "elearning",
+  port: "3306",
+  password: "elearningpw",
+  database: "keiwan",
+  multipleStatements: true
 });
 
 connection.connect();
 
 // Execute the database initialisation
-connection.query(fs.readFileSync('sql/init.sql').toString(), function (error, results, fields) {
-    if (error) throw error;
-    connection.query(fs.readFileSync('sql/testdata.sql').toString(), function (error, results, fields) {
-        if (error) throw error;
-    });
+connection.query(fs.readFileSync("sql/init.sql").toString(), function(
+  error,
+  results,
+  fields
+) {
+  if (error) throw error;
 });
 
+connection.query(fs.readFileSync("sql/testdata.sql").toString(), function(
+  error,
+  results,
+  fields
+) {
+  if (error) throw error;
+});
 
 module.exports = connection;
