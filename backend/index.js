c41ded750cc55b61c90ec519b9ac9394855a2c3e
Bomel <33158209+Bomel@users.noreply.github.com>
2019-10-06 23:02:16 +0200

    removed some database testing rows

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,9 @@
 const express = require('express')
-var sql = require('./sql/db.js');
 const app = express()
 const port = 3000
 
 // import routing
 var user = require('./src/user');
-
-sql.query('INSERT INTO `User` (`s-Nummer`) VALUES("s6032689");')
-
-sql.query('SELECT * FROM User;', function (error, results, fields) {
-    if (error) throw error;
-    console.log('Inside the user table is:: ', results[0]);
-});
 
 app.use('/user', user);
 
