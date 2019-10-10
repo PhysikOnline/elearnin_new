3256e5231110e9bdfdde1a0985d325c6e7a3a2dc
Bomel <keiwanjamaly@protonmail.com>
2019-10-10 02:50:03 +0200

    Added Sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./App.css";
 
+import Toolbar from "./Toolbar/Toolbar";
+import SideDrawer from "./SideDrawer/SideDrawer";
+import Backdrop from "./Backdrop/Backdrop";
+
 class App extends React.Component {
+  state = {
+    sideDrawerOpen: false
+  };
+  sideDrawerToggleClickHandler = () => {
+    this.setState(previousState => {
+      return { sideDrawerOpen: !previousState.sideDrawerOpen };
+    });
+  };
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
   render() {
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
+    }
+
     return (
       <div className="App">
+        <Toolbar drawerClickHandler={this.sideDrawerToggleClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
         <p>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
